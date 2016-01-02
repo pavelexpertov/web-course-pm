66cfdb1d081a17da9085b65034b9a364825a6f3a
pavelexpertov <pavelm01@hotmail.co.uk>
2016-01-02 23:45:29 +0000

    Created one file and edited another file
    --Created forthcoming_events.inc.php, but it needs more implementation
    --Added an introduction paragraph to introduction_section.inc.php

---
+++ php/includes/web_comp/forthcoming_events.inc.php
@@ -0,0 +1,3 @@
+<aside>
+You need to fill in some database code lolz    
+</aside>
--- php/includes/web_comp/introduction_section.inc.php
+++ php/includes/web_comp/introduction_section.inc.php
@@ -1,6 +1,12 @@
 <section id="introduction">
     <!-- This section represents different types of events
     that are stored within small boxes -->
+    <p>
+        Welcome to the CoderCon! Here you can book tickets for workshops and conferences.
+        Here is a list of boxes that describe each type of a conference or you can search
+        for events using the search bar.
+    </p>
+
     <?php
         include 'php/web_comp/small_info_box.func.php';
         $listOfEventTypes[] = array('Header 1', 'Desciption 1');
