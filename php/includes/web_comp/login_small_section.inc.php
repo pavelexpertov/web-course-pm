4106be63df5a80298072ea0d46f340459aa742a8
pavelexpertov <pavelm01@hotmail.co.uk>
2016-01-04 14:01:50 +0000

    Created a file that posts login section based on session variable

---
+++ php/includes/web_comp/login_small_section.inc.php
@@ -0,0 +1,13 @@
+<div id="login-section">
+    <?php
+    if(!isset($_SESSION['user'])){ ?>
+
+    <a href='#'>Login (click)</a>
+    
+    <?php
+} else { ?>
+
+    <p>You logged in?</p>
+
+    <?php }//End of the else clause?>
+</div>
