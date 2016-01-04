96425a3196f21abd658bd6dc0792a2479738077b
pavelexpertov <pavelm01@hotmail.co.uk>
2016-01-04 12:02:55 +0000

    Fixed an issue with forthcoming events

--- php/includes/web_comp/forthcoming_events.inc.php
+++ php/includes/web_comp/forthcoming_events.inc.php
@@ -1,6 +1,29 @@
-<aside>
+<aside id="forthcoming_events">
 <!-- The purpose of this page is to display
 closes events (displaying their names, dates
 and locations) -->
-You need to fill in some database code lolz
+
+    <?php
+        //Getting conection to the database
+        require_once 'php/conn_sess/dbconn.inc.php';
+
+        $queryEarlyEvents = "select ename, date, time,
+                            town, country
+                            from forthcoming_events";
+        $resultEarlyEvents = $mysqli->query($queryEarlyEvents);
+    ?>
+
+    <?php
+        while($event = $resultEarlyEvents->fetch_assoc()){
+    ?>
+    <div class="forth_events">
+        <ul>
+            <li> <?php echo $event['ename']   ?> </li>
+            <li> <?php echo $event['date']   ?> </li>
+            <li> <?php echo $event['time']   ?> </li>
+            <li> <?php echo $event['town']    ?></li>
+            <li> <?php echo $event['country']    ?></li>
+        </ul>
+    </div>
+    <?php }//End of the while loop?>
 </aside>
