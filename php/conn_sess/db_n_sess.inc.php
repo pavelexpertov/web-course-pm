7a4e2e9e5a5043c2c07807923dd46b9cdb90ce19
pavelexpertov <pavelm01@hotmail.co.uk>
2016-01-04 13:46:35 +0000

    Created two files in php/conn_sess
    --Created sess.inc.php to start a session
    --Created db_n_sess.inc.php include file to start the session and include database connection

---
+++ php/conn_sess/db_n_sess.inc.php
@@ -0,0 +1,4 @@
+<?php
+include 'php/conn_sess/sess.inc.php';
+include 'php/conn_sess/dbconn.inc.php';
+?>
---
+++ php/conn_sess/sess.inc.php
@@ -0,0 +1,5 @@
+<?php
+    //The purpose of the file is to start a session and it
+    //may have custom cookie settings
+    session_start();
+ ?>
