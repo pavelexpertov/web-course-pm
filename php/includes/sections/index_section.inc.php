7f748434a7dc07988aed3105012e69c30a5d23a8
pavelexpertov <pavelm01@hotmail.co.uk>
2016-01-04 15:35:49 +0000

    Created an include that places a whole section for index.php page

---
+++ php/includes/sections/index_section.inc.php
@@ -0,0 +1,10 @@
+<!-- This pages's purpose is to represent elements on the index.php page -->
+<div>
+    <?php
+        //Including the forthcoming events
+        include "php/includes/web_comp/forthcoming_events.inc.php";
+        //including introduction section
+        include 'php/includes/web_comp/introduction_section.inc.php';
+    ?>
+
+</div>
