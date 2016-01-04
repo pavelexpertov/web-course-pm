cfd8f5b10bee2374676faabdd89a5d22bc2f888c
pavelexpertov <pavelm01@hotmail.co.uk>
2016-01-04 13:17:03 +0000

    Created an include file that will place header section
    --The file is placed in php/includes/web_comp
    --The file needs two more includes to place login and simple search bar section.

---
+++ php/includes/web_comp/header_section.inc.php
@@ -0,0 +1,19 @@
+<!-- This is a header section that will contain:
+- Logo
+- Login section (which needs to be implemented so it's either a button or user's name as an include file)
+- navigation link to different parts of the sites
+- a simple search bar (which needs to be implemented as an include) -->
+<header>
+    <img id="header_logo"></img>
+    <nav>
+        <ul>
+            <li> <a href="#">Home</a></li>
+            <li> <a href="#">Sites</a></li>
+            <li> <a href="#">Speakers</a></li>
+        </ul>
+    </nav>
+
+    <!-- The login section goes here -->
+    <!-- The simple search bar goes here -->
+
+</header>
