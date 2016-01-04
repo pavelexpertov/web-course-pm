ab6cf83ff7b88ff37ab56dc77a30e0cd9565fdae
pavelexpertov <pavelm01@hotmail.co.uk>
2016-01-04 10:59:54 +0000

    just making a commit before merging into external branch

--- php/cms/retrieve/get_forthcoming_events.php
+++ php/cms/retrieve/get_forthcoming_events.php
@@ -1,14 +1,27 @@
 <?php
+// This include retrieves top 5 early events from the database
     require_once 'php/conn_sess/dbconn.inc.php';
 
-    echo "Listen, the messages you get needs to be removed";
-    echo "\nBecause you need to make this file get \n
-        latest events rathen than pavelexpertov and what not";
+    // echo "Listen, the messages you get needs to be removed";
+    // echo "\nBecause you need to make this file get \n
+    //     latest events rathen than pavelexpertov and what not";
 
-    $query = "select * from Users";
-    $result = $mysqli->query($query);
+    //  $query = "select ID, Username from Users";
+    //  $result = $mysqli->query($query);
+     //
+    //  $user = $result->fetch_assoc();
+    //  echo "{$user['ID']}";
+    //  echo "{$user['Username']}";
 
-    $user = $result->fetch_assoc();
-    echo "{$user['ID']}";
-    echo "{$user['Username']}";
+    /*$queryEarlyEvents = "select ID, Name, Date, VenueID
+                        from Events order by Date
+                        limit 10";
+    $resultEarlyEvents = $mysqli->query($queryEarlyEvents);*/
+
+    $queryEarlyEvents = "select name from forth";
+    $resultEarlyEvents = $mysqli->query($queryEarlyEvents);
+    $event = $resultEarlyEvents->fetch_assoc();
+    echo $event['name'];
+
+
 ?>
