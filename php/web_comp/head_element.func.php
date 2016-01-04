8d133812937a0eb8573256d4a382b09c0c1c7e30
pavelexpertov <pavelm01@hotmail.co.uk>
2016-01-04 12:23:35 +0000

    Created a file that will place a <head> element within sites
    --Created a file named head_element.func.php in web_comp

---
+++ php/web_comp/head_element.func.php
@@ -0,0 +1,12 @@
+<?php
+    function placeHeadElement($title)
+    {
+        ?>
+        <!doctype html>
+        <head>
+
+            <title> <?php echo $title; ?> </title>
+
+        </head>
+
+<?php }//End of the placeHeadElement function ?>
