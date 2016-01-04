082ed8eb630f13bc073e7293570231963e22c230
pavelexpertov <pavelm01@hotmail.co.uk>
2016-01-04 22:46:35 +0000

    Just done the speakerinfo_section.inc.php Now I am jsut trying to fix it

--- php/includes/web_comp/speakerinfo_section.inc.php
+++ php/includes/web_comp/speakerinfo_section.inc.php
@@ -1,1 +1,13 @@
 <section>
+    <?php
+    $query = "select fname, lname, job, desc from speaker_info
+              where id = ?";
+    $result = $mysqli->prepare($query);
+    $result->bind_param('i', $_GET['id']);
+    $result->execute();
+    $result->bind_result($fname, $lname, $job, $desc);
+    $result->fetch();
+    $result->close();
+    echo "$fname" . "$lname";
+    ?>
+</section>
