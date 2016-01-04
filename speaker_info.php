6ed303060e77d7ac182aef5dd3e8c9815b0cc7ff
pavelexpertov <pavelm01@hotmail.co.uk>
2016-01-04 21:21:27 +0000

    Created speaker_info.php

---
+++ speaker_info.php
@@ -0,0 +1,11 @@
+<!-- This is speaker_info page -->
+<?php
+    include 'php/conn_sess/db_n_sess.inc.php';
+
+    //Adding head element
+    include 'php/web_comp/head_element.func.php';
+    include 'php/web_comp/body_element.func.php';
+    placeHeadElement("Speaker's information");
+
+    $includeList[] = 'php/includes/sections/header_section.inc.php';
+?>
