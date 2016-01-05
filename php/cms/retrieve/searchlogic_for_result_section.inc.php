b9d2a023467b46548bc4eb4f527f688d83f0fff4
pavelexpertov <pavelm01@hotmail.co.uk>
2016-01-05 22:53:52 +0000

    edited the searchlogic file and created a view with new sql select statment

--- php/cms/retrieve/searchlogic_for_result_section.inc.php
+++ php/cms/retrieve/searchlogic_for_result_section.inc.php
@@ -5,12 +5,18 @@
     //The variable below will be passed to a function, which will
     //use it to output values.
     $resultEvents = 'sdf';
+
+    //These variables are used so I can customise the query easily
+    //in mulitple if conditions
+    $fields = " evename";
+    $view = "search_n_event_view";
+
     if(isset($_GET['simplesearch']))
     {
         echo "{$_GET['simplesearch']}";
         echo "It worked, it worked!!";
 
-        $query = "select evename from search_view where eveid = ?";
+        $query = "select $fields from $view where eveid = ?";
         $resultEvents = $mysqli->prepare($query);
         $resultEvents->bind_param('i', $_GET['simplesearch']);
         $resultEvents->execute();
