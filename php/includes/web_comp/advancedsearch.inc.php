f9fa6e786883604b2e59cf36db6acf21bb0e11d5
pavelexpertov <pavelm01@hotmail.co.uk>
2016-01-05 14:47:04 +0000

    Created search.php page and added a simple for for advancedsearch.inc.php

---
+++ php/includes/web_comp/advancedsearch.inc.php
@@ -0,0 +1,8 @@
+<!-- This is a search section where form elements (i.e. searchbar) are placed. -->
+<div id="adv-search">
+    <form action='search.php' method='get'>
+        <label for="adv-search"> Search for events here</label>
+        <input type="text" name="adv-search" id="adv-search">
+        <input type="submit" value="Search">
+    </form>
+</div>
