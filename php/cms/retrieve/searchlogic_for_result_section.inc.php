b8035144b67a3580b381c7fab891979bf74fa2f9
pavelexpertov <pavelm01@hotmail.co.uk>
2016-01-05 16:10:04 +0000

    Created a prepared statement within searchlogic_for_resutl_section include and now I am going to improve it to search for content

--- php/cms/retrieve/searchlogic_for_result_section.inc.php
+++ php/cms/retrieve/searchlogic_for_result_section.inc.php
@@ -2,10 +2,22 @@
 // The purpose of this page is to make a search query based on
 // query that came either from simple or advanced search bar.
 
+    //The variable below will be passed to a function, which will
+    //use it to output values.
+    $resultEvents = 'sdf';
     if(isset($_GET['simplesearch']))
     {
         echo "{$_GET['simplesearch']}";
         echo "It worked, it worked!!";
+
+        $query = "select evename from search_view where eveid = ?";
+        $resultEvents = $mysqli->prepare($query);
+        $resultEvents->bind_param('i', $_GET['simplesearch']);
+        $resultEvents->execute();
+        $resultEvents->bind_result($name);
+        $resultEvents->fetch();
+
+        echo "The result of the query is $name";
     }
     else
     {
