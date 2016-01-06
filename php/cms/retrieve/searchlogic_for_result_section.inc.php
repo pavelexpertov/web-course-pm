249b31014574481db77dba328401db1d1b6829eb
pavelexpertov <pavelm01@hotmail.co.uk>
2016-01-06 00:05:01 +0000

    Done with the logic and function, gonna try to improve now

--- php/cms/retrieve/searchlogic_for_result_section.inc.php
+++ php/cms/retrieve/searchlogic_for_result_section.inc.php
@@ -2,13 +2,15 @@
 // The purpose of this page is to make a search query based on
 // query that came either from simple or advanced search bar.
 
+    //The include gives a function that will output the result of the events.
+    include 'php/web_comp/result_events.func.php';
     //The variable below will be passed to a function, which will
     //use it to output values.
     $resultEvents = 'sdf';
 
     //These variables are used so I can customise the query easily
     //in mulitple if conditions
-    $fields = " evename";
+    $fields = " eveid, evename, etypename, date, stime, ftime, town, country, price";
     $view = "search_n_event_view";
 
     if(isset($_GET['simplesearch']))
@@ -20,10 +22,10 @@
         $resultEvents = $mysqli->prepare($query);
         $resultEvents->bind_param('i', $_GET['simplesearch']);
         $resultEvents->execute();
-        $resultEvents->bind_result($name);
-        $resultEvents->fetch();
+        //$resultEvents->fetch();
 
-        echo "The result of the query is $name";
+        placeEventResult($resultEvents);
+        // echo "The result of the query is $name";
     }
     else
     {
---
+++ php/web_comp/result_events.func.php
@@ -0,0 +1,30 @@
+<?php
+//This file is used to make a function that
+//will take a result prepared statement and print
+//info in the correct output using a loop.
+
+function placeEventResult(&$queryResult)
+{
+    $queryResult->bind_result($eveid, $name, $etype, $date, $stime, $ftime, $town, $country, $price);
+    while($queryResult->fetch()) {
+?>
+
+<div class='eventresult'>
+    <h3>
+        <?php echo $name; ?>
+    </h3>
+
+    <ul>
+        <li> Event Type: <?php echo $etype?> </li>
+        <li> Date: <?php echo $date?> </li>
+        <li> Start Time: <?php echo $stime?> </li>
+        <li> Finish Time: <?php echo $ftime ?> </li>
+        <li> Location: <?php echo $town . ", " . $country ?> </li>
+        <li> Price: <?php echo $price ?> </li>
+    </ul>
+</div>
+
+<?php
+ } //End of the while loop
+} //The end of the placeEventResult function
+?>
