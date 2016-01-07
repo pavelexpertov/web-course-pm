72107baf0d1e8b37541f32db0c6394536dba9ec4
pavelexpertov <pavelm01@hotmail.co.uk>
2016-01-07 15:46:49 +0000

    Created a function for includes and solved some problems, now I am going to finish things up

--- php/cms/verifydata/check_logic_for_login.inc.php
+++ php/cms/verifydata/check_logic_for_login.inc.php
@@ -1,6 +1,16 @@
 <?php
     //This is for error reporting
-    include 'php/debug/turn_on_error_reporting.php';
+    // include '../../debug/turn_on_error_reporting.php';
+    include '../../web_comp/place_sess_db_include.func.php';
+    $list = placeSDIncludes(2);
+
+    foreach($list as $include)
+    {
+        include $include;
+    }
+
+    //This is for database stuff
+    // include '../../conn_sess/db_n_sess.inc.php';
     //Clear out the error variable
     if(isset($_SESSION['err']))
         unset($_SESSION['err']);
@@ -9,7 +19,7 @@
     if(!isset($_POST['usr']) || !isset($_POST['pwd']))
     {
         $_SESSION['err'] = "Ooops, not enough variables been passed";
-        header("Location: $_SERVER['HTTP_REFERER']");
+        header("Location: {$_SERVER['HTTP_REFERER']}");
         exit();
     }
 
@@ -18,7 +28,7 @@
     $querystmt->bind_param('i', $_POST['usr']);
     if($querystmt == false){
         $_SESSION['err'] = "Some problem with prepared statement " . $querystmt->error_list;
-        header("Location: $_SERVER['HTTP_REFERER']");
+        header("Location: {$_SERVER['HTTP_REFERER']}");
         exit();
     }
     $querystmt->execute();
@@ -27,18 +37,21 @@
     $usr_exist = $querystmt->num_rows;
     if($usr_exist == 0)
     {
-        $_SESSION['err'] = "Unfortunately incorrect username or password. Try again.";
-        header("Location: $_SERVER['HTTP_REFERER']");
+        $_SESSION['err'] = "the values of usr_exit is $usr_exist";
+        // $_SESSION['err'] = "Unfortunately incorrect username or password. Try again.";
+        header("Location: {$_SERVER['HTTP_REFERER']}");
         exit();
     }
 
     $querystmt->bind_result($usrid, $usrname, $usrpwd);
     $querystmt->fetch();
-    $querystmt->close()
+    $querystmt->close();
     if($_POST['pwd'] != $usrpwd)
     {
         $_SESSION['err'] = "Unfortunately incorrect username or password. Try again.";
-        header("Location: $_SERVER['HTTP_REFERER']");
+        header("Location: {$_SERVER['HTTP_REFERER']}");
+        // echo "usrpwd from post value is: {$_POST['pwd']} <br>";
+        // echo "usrpwd from query is: $usrpwd <br>";
         exit();
     }
 
