d2e75263ad3a98c8c6fdbcbe54aabe21ee5c89d5
pavelexpertov <pavelm01@hotmail.co.uk>
2016-01-07 13:36:04 +0000

    Just created the files for development

---
+++ login.php
@@ -0,0 +1,17 @@
+<?php
+
+    //Creating database connection and starting session
+    include 'php/conn_sess/db_n_sess.inc.php';
+    //Adding head element and body elements
+    include 'php/web_comp/head_element.func.php';
+    include 'php/web_comp/body_element.func.php';
+    placeHeadElement("Login");
+
+    //Adding header element
+    $includeList[] = 'php/includes/sections/header_section.inc.php';
+    //Adding section designated for login.php
+    // $includeList[] = 'php/includes/sections/index_section.inc.php';
+
+    placeBodyElement($includeList);
+
+ ?>
---
+++ php/includes/sections/loginpage_section.inc.php
@@ -0,0 +1,8 @@
+<div>
+    <?php
+        //Including a login section.
+        // include 'php/includes/web_comp/...';
+
+     ?>
+
+</div>
