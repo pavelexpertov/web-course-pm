8b73655ebd331139debbadfee8b651635db26ee8
pavelexpertov <pavelm01@hotmail.co.uk>
2016-01-10 12:58:59 +0000

    Finally fixed the log out problem, now tidying up

--- logout_logic.php
+++ logout_logic.php
@@ -2,15 +2,15 @@
     //The purpose of the file is to destroy a session when a user wants to logout
     //from the website
 
-    session_set_cookie_params(0, '../../');
-    session_start();
-    include '../obj/user.cs.php';
+    // session_set_cookie_params(0, '../../');
+    // session_start();
+    // include '../obj/user.cs.php';
     //Started a session with custom properties
-    // include 'sess.inc.php';
+    include 'php/conn_sess/sess.inc.php';
     //If the request is not from logged in user, redirect to original destination
     if(!isset($_SESSION['usr']))
     {
-        header("Location: {$_SERVER['HTTP_REFERER']}");
+        // header("Location: {$_SERVER['HTTP_REFERER']}");
         echo $_SERVER['HTTP_REFERER'];
         echo "Ehhhhhhhh, something went wrong, if you logged in and wanted to
          log out, the condition must not have found the 'usr' variable in session";
@@ -22,6 +22,6 @@
     $_SESSION =  array();
     session_destroy();
     setcookie('PHPSESSID', time()-3600);
-    header("Location: ../../index.php");
+    header("Location: index.php");
 
  ?>
