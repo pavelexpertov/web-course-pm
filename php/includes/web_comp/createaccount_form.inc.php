f61bd800fe9adbcd24e4faa118d3f36e541c844e
pavelexpertov <pavelm01@hotmail.co.uk>
2016-01-11 21:26:08 +0000

    Just entered some forms, now testing them

--- php/includes/web_comp/createaccount_form.inc.php
+++ php/includes/web_comp/createaccount_form.inc.php
@@ -1,5 +1,23 @@
 <section>
+    <form action="" method="post">
+        <label for="usrname">Enter your username</label>
+        <input type="text" name="usrname" id="usrname">
 
+        <label for="usrpwd">Enter your username</label>
+        <input type="text" name="usrpwd" id="usrpwd">
+
+        <label for="bio_cbx">Enter your username</label>
+        <input type="checkbox" name="bio_cbx" id="bio_cbx">
+
+        <textarea id="biodesc" name="biodesc"> </textarea>
+
+        <input type="submit" value="Create an account">
+
+    </form>
+
+    <div class="error-section">
+
+    </div>
 
 
 </section>
