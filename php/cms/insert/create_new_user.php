89e437ad872a1c8796e9c19295e28d11d6816a58
pavelexpertov <pavelm01@hotmail.co.uk>
2016-01-11 22:40:31 +0000

    Made major progress but I cant make the damn thing insert new data into the database

--- php/cms/insert/create_new_user.php
+++ php/cms/insert/create_new_user.php
@@ -7,6 +7,11 @@ $listOfIncludes = placeSDIncludes(2);
 foreach($listOfIncludes as $include)
     include $include;
 
+//Clear the error message just in case
+if(isset($_SESSION['err']))
+{
+    unset($_SESSION['err']);
+}
 //First stage is to collect some needed info to check against (i.e. existing username)
 $query = "select Username from Users where Username = ?";
 $usrnamestmt = $mysqli->prepare($query);
@@ -14,22 +19,38 @@ $usrnamestmt->bind_param('s', $_POST['usrname']);
 $usrnamestmt->execute();
 $usrnamestmt->store_result();
 $numOfQueries = $usrnamestmt->num_rows;
-$usrnamestmt->bind_result($existusrname);
+//Second stage is to compare the usrname against the post's
 if($numOfQueries == 1)
 {
-    $_SESSION['err'] = "The username already exists";
+    $usrnamestmt->bind_result($existusrname);
+    if($existusrname == $_SESSION['usrname'])
+        $_SESSION['err'] = "The username already exists";
     header("Location: {$_SERVER['HTTP_REFERER']}");
     exit();
 }
 elseif($numOfQueries > 1)
 {
-    $_SESSION['err'] = "For some reason you got more than one query you asked for, for username";
+    $_SESSION['err'] = "For some reason you got more than one query you asked for, for username or email";
     header("Location: {$_SERVER['HTTP_REFERER']}");
     exit();
 }
 
-//Second stage is to compare the usrname against the post's
-
+//If all passed, you can insert the info into the database
+$query = "insert into Users(ID, Username, Password, Description)
+          values(?, ?, ?, ?)";
+$insertstmt = $mysqli->prepare($query);
+$a = 0;
+$insertstmt->bind_param('isss', $a, $_SESSION['usrname'], $_SESSION['usrpwd'],
+                        $_SESSION['biodesc']);
+if($insertstmt == false)
+{
+    echo "Oppppps, error happened while inserting";
+    exit();
+}
+$insertstmt->execute();
+$insertstmt->close();
+$_SESSION['err'] = "successfully created an account";
+header("Location: {$_SERVER['HTTP_REFERER']}");
 
 
  ?>
