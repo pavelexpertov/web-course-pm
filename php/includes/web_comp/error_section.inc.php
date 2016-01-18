5a73576cfe701f0f927c38531cfb723b40baaccf
pavelexpertov <pavelm01@hotmail.co.uk>
2016-01-18 20:00:10 +0000

    Created some files to start off implementing a user page:
    --Created error_section.inc.php to include a section to display session variable 'err'
    --Created usrpage_section.inc.php to contain web components for the user to use.
    --Created bookedeventslist.inc.php to display booked events.
    --Created usersettings to diplay user settings.
    --Created check_if_user_legit so it can throw an error when a user that is not logged in.
    --Created usr_page.php so that a logged in user can use the page to add and edit events.

---
+++ php/includes/web_comp/error_section.inc.php
@@ -0,0 +1,9 @@
+<div class="error-section">
+        <?php
+            if(isset($_SESSION['err']))
+            {
+                echo "{$_SESSION['err']}";
+            }
+
+         ?>
+</div>
---
+++ php/usr/verify/check_if_user_legit.inc.php
@@ -0,0 +1,12 @@
+<?php
+//The purpose of the file is to kick out the request if the guy does not have a legitimate
+//session. Also it destroys err keyword in the session array.
+if(isset($_SESSION['err']))
+    unset($_SESSION['err']);
+
+if(!isset($_SESSION['usr']))
+{
+    $_SESSION['err'] = "Oooops something happened in the process of kicking out unwanted people";
+    header("Location: {$_SERVER['HTTP_REFERER']}");
+}
+ ?>
