ed9572badc2d82c19543d21bb28d8dd6033c6b92
pavelexpertov <pavelm01@hotmail.co.uk>
2016-01-21 23:43:49 +0000

    Just edited usrpage_seciton.inc.php before pulling the branch

--- php/usr/includes/sections/usrpage_section.inc.php
+++ php/usr/includes/sections/usrpage_section.inc.php
@@ -1,16 +1,11 @@
 <div>
-    <!-- The links are for the javascript interactivity with sections. -->
-    <ul>
-        <li>Booked Events</li>
-        <li>Created Events</li>
-        <li>User Settings</li>
-    </ul>
+    
+<?php
+    //The section is used for the user to edit events, manage
+    //bookings and edit user settings.
+    //Includes a list of created events
+    include 'php/usr/web_comp/createdeventslist.inc.php';
 
-    <?php
-        include 'php/inlcudes/web_comp/error_section.inc.php';
-        include 'php/usr/web_comp/bookedeventslist.inc.php';
-        include 'php/usr/web_comp/createdeventslist.inc.php';
-        include 'php/usr/web_comp/usersettings.inc.php';
-    ?>
+ ?>
 
 </div>
