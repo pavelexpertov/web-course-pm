57b199e7074e092aca4ed16b118b1f93e7313388
pavelexpertov <pavelm01@hotmail.co.uk>
2016-02-04 15:12:44 +0000

    Just created some files to make sections and a form

---
+++ php/usr/includes/sections/create_event_section.inc.php
@@ -0,0 +1,6 @@
+<div>
+  <?php
+    //the purpose of the section is to allow user to create a new event
+    include 'php/usr/includes/web_comp/create_event_form.inc.php';
+  ?>
+</div>
---
+++ php/usr/includes/web_comp/create_event_form.inc.php
@@ -0,0 +1,12 @@
+<form action="" method="post">
+    <fieldset>
+      <legend>Create a new event</legend>
+      <label>Event name</label>
+      <input type="text" id="evename" name="evename">
+      <label>Event description</label>
+      <textarea id="descr" name="descr"> </textarea>
+      <input type="sumbit" value="Create a new event">
+    </fieldset>
+
+
+</form>
