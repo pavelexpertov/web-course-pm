92914417748a202190b0ed34e068e7b111a1cbd8
pavelexpertov <pavelm01@hotmail.co.uk>
2016-02-05 10:57:31 +0000

    Created the first stylesheet and some rules for the main wrapper and the header

--- php/web_comp/head_element.func.php
+++ php/web_comp/head_element.func.php
@@ -7,7 +7,7 @@
         <head>
 
             <title> <?php echo $title; ?> </title>
-
+            <link rel="stylesheet" type="text/css" href="css/style.css">
         </head>
 
 <?php }//End of the placeHeadElement function ?>
