e374361484354e688c58ec641b234e696b9b2875
pavelexpertov <pavelm01@hotmail.co.uk>
2016-02-10 22:13:09 +0000

    Just added forthcoming events to the event information

--- php/includes/sections/eventinfo_section.inc.php
+++ php/includes/sections/eventinfo_section.inc.php
@@ -1,5 +1,6 @@
 <div class="middle-section">
     <?php
+        include "php/includes/web_comp/forthcoming_events.inc.php";
         include 'php/includes/web_comp/event_info.inc.php';
     ?>
 </div>
