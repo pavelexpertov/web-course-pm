bcbf77e01e5c96e8c261a452976288cbe5f65668
pavelexpertov <pavelm01@hotmail.co.uk>
2016-02-13 20:51:15 +0000

    Created a function that prints out venue details

---
+++ php/web_comp/venue_ddlist.func.php
@@ -0,0 +1,51 @@
+<?php
+//The purpose of the function is to print out
+//a drop down list that will contain venue's info and id
+
+//Creating an associative array of data in an array
+function placeDDVenues($ddname, $selectedVenue = false){
+    include 'php/conn_sess/dbconn.inc.php';
+    $listOfVenues = array();
+    $query = "select ID, Name, Town, Country
+              from Venues where Archived = 0";
+    $stmt = $mysqli->query($query);
+
+    while($v = $stmt->fetch_assoc())
+    {
+        $venue = array("name"=>$v['Name'],
+                        "id"=>$v['ID'],
+                        "town"=>$v['Town'],
+                        "country"=>$v['Country']);
+        $listOfVenues[] = $venue;
+    }
+
+
+        print_r($listOfVenues);
+
+
+    //Placing the drop down list
+    ?>
+<select id="<?php echo $ddname;?>" name="<?php echo $ddname;?>">
+    <?php
+    foreach($listOfVenues as $v)
+    {
+        $info = "Name: {$v['name']}, Town: {$v['town']}, Country: {$v['country']}";
+        if($selectedVenue != false && $v['id'] == $selectedVenue)
+        {
+        ?>
+        <option value="<?php echo $v['id'];?>" selected> <?php echo $info; ?> </option>
+
+        <?php
+        }//End of the if clause
+        else
+        {
+            ?>
+        <option value="<?php echo $v['id'];?>"> <?php echo $info; ?> </option>
+            <?php
+        }//End of the else clause
+    }//End of the foreach loop
+    ?>
+</select>
+    <?php
+}//End of the function placeDDVenue
+ ?>
