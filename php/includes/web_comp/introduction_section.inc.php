05e412ea086e1879f0b90184f7312c6dbd9374fe
pavelexpertov <pavelm01@hotmail.co.uk>
2016-02-14 08:41:28 +0000

    Shortened introduction page and removed a list of event types

--- php/includes/web_comp/introduction_section.inc.php
+++ php/includes/web_comp/introduction_section.inc.php
@@ -2,20 +2,9 @@
     <!-- This section represents different types of events
     that are stored within small boxes -->
     <p>
-        Welcome to the CoderCon! Here you can book tickets for workshops and conferences.
-        Here is a list of boxes that describe each type of a conference or you can search
-        for events using the search bar.
+        Welcome to the CoderCon! Here you can book tickets for workshops and conferences that
+	are made for developers, technical administrators and tech hobbyists.
     </p>
 
-    <?php
-        include 'php/web_comp/small_info_box.func.php';
-        $listOfEventTypes[] = array('Header 1', 'Desciption 1');
-        $listOfEventTypes[] = array('Header 2', 'Desciption 2');
-
-        foreach($listOfEventTypes as $eventType)
-        {
-            printSmallInfoBox($eventType[0],$eventType[1]);
-        }
-    ?>
 
 </section>
