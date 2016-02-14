7ddd082964f0d7d93b7e23a7e7cc891f640343d7
pavelexpertov <pavelm01@hotmail.co.uk>
2016-02-14 16:35:51 +0000

    Just made the edit of events work

--- php/cms/update/update_created_event.php
+++ php/cms/update/update_created_event.php
@@ -6,10 +6,26 @@
   $list = placeSDIncludes(2, true);
   foreach($list as $include)
     include $include;
-    
+
   //The statement updates existing entries for event's data
   //The event's event's name and start time are updated
-  $query = "update Events set Name = ?, Description = ?
+
+  $eveid = $_POST['eid'];
+  $evename = $_POST['evename'];
+  $descr = $_POST['descr'];
+  $stime = $_POST['stime'];
+  $ftime = $_POST['ftime'];
+  $date = convertDate($_POST['date']);
+  $evetypeid = $_POST['evetype'];
+  $venueid = $_POST['venueid'];
+
+  $listOfVars = array($eveid, $evename, $descr, $date, $stime, $ftime,
+                        $evetypeid, $venueid);
+
+  $columns = "Name = ?, Description = ?,
+              Date = ?, StartTime = ?, FinishTime = ?,
+              ConfType = ?, VenueID = ?";
+  $query = "update Events set $columns
             where EManagerID = ? and ID = ?";
 
   $stmt = $mysqli->prepare($query);
@@ -18,8 +34,10 @@
       echo "Ooop an error has happened at prepare statement line";
       exit();
   }
-  $stmt->bind_param("ssii", $_POST['evename'], $_POST['descr'],
-                    $_SESSION['usr']->id, $_POST['eid']);
+  $stmt->bind_param("sssssiiii", $evename, $descr,
+                    $date, $stime, $ftime,
+                    $evetypeid, $venueid,
+                    $_SESSION['usr']->id, $eveid);
   $stmt->execute();
   $stmt->close();
   //Return the user to the userpage
