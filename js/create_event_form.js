e75b16465a8f3beb9397ff0ec5b8906a80bb0668
pavelexpertov <pavelm01@hotmail.co.uk>
2016-02-14 18:57:56 +0000

    just finished with jquery validation for create events form

--- js/create_event_form.js
+++ js/create_event_form.js
@@ -5,14 +5,22 @@ $("#create-event").validate({
             required: true,
             maxlength: 24
         },
-        descr: "required"
+        descr: "required",
+        date: {
+            required: true,
+            date: true
+        }
     },
     messages: {
         evename: {
             required: "You must type in name for the event",
             maxlength: "you can only type up to 24 characters"
         },
-        descr: "You must enter event description"
+        descr: "You must enter event description",
+        date: {
+            required: "you must enter a date for your event",
+            date: "You must enter in dd/mm/yyyy format"
+        }
     }
 });
 
