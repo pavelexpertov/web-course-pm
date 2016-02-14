f34bac8ae8f716534ca9ebc146503f3554273cb7
pavelexpertov <pavelm01@hotmail.co.uk>
2016-02-14 15:50:22 +0000

    Edited the form for editing events

--- php/usr/includes/web_comp/edit_createdevent_form.inc.php
+++ php/usr/includes/web_comp/edit_createdevent_form.inc.php
@@ -1,19 +1,16 @@
-<!-- <form action="php/cms/update/update_created_event.php" method="post">
-  <fieldset>
-    <legend>Edit your event</legend>
-    <label> Event Name</label>
-    <input type="text" value = "This is the name to edit for" >
-    <textarea>This is a text to edit </textarea>
-    <input type="submit" value="Send the changes">
-  </fieldset>
-
-</form> -->
-
 <?php
   //The purpose of the page is to get information about existing event
   //and enter it into the form field elements for the user to edit
   //Creating a query
-  $query = "select Name, Description from Events where ID = ?";
+    include 'php/web_comp/time_ddlist.func.php';
+    include 'php/web_comp/venue_ddlist.func.php';
+    include 'php/web_comp/event_type_ddlist.func.php';
+  $eveid = $_GET['eid'];
+
+  $columns = "Name, Description,
+              Date, StartTime, FinishTime,
+              ConfType, VenueID";
+  $query = "select $columns from Events where ID = ?";
   $stmt = $mysqli->prepare($query);
   if($stmt == false)
   {
@@ -21,7 +18,7 @@
     echo $stmt->error_list;
     exit();
   }
-  $stmt->bind_param('i', $_GET['eid']);
+  $stmt->bind_param('i', $eveid);
   $stmt->execute();
 
   //Checking for a number of queries
@@ -33,19 +30,56 @@
     exit();
   }
 
-  $stmt->bind_result($evename, $evedescr);
+  $stmt->bind_result($evename, $evedescr,
+                     $date, $stime, $ftime,
+                     $evetype, $venueid );
   $stmt->fetch();
   $stmt->close();
 
  ?>
+
+
 <form action="php/cms/update/update_created_event.php" method="post">
+    <fieldset>
+      <legend>Create a new event</legend>
+      <label>Event name</label>
+      <input type="text" id="evename" name="evename" value="<?php echo $evename;?>">
+      <br>
+      <label>Event description</label>
+      <textarea id="descr" name="descr"> <?php echo $evedescr;?></textarea>
+      <br>
+      <label>Date</label>
+      <input type="text" id="date" name="date" value="<?php echo $date;?>">
+      <br>
+      <label>Start Time</label>
+      <?php placeDDTime("stime", $stime); ?>
+      <br>
+      <label>Finish Time</label>
+      <?php placeDDTime("ftime", $ftime); ?>
+      <br>
+      <label>Event Type</label>
+      <?php placeDDEventTypes("evetype"); ?>
+      <br>
+      <label>Venue</label>
+      <?php placeDDVenues("venueid"); ?>
+      <br>
+      <input type="hidden" name="eid" id="eid" value="<?php echo $eveid; ?>">
+      <input type="submit" value="Save changes">
+    </fieldset>
+
+
+</form>
+
+
+
+
+<!-- <form action="php/cms/update/update_created_event.php" method="post">
   <fieldset>
     <legend>Edit your event</legend>
     <label> Event Name</label>
     <input type="text" name="evename" id="evename" value = "<?php echo $evename; ?>" >
     <textarea name="descr" id="descr"><?php echo $evedescr; ?> </textarea>
-    <input type="hidden" name="eid" id="eid" value="<?php echo $_GET['eid']; ?>">
+    <input type="hidden" name="eid" id="eid" value="<?php echo $eveid; ?>">
     <input type="submit" value="Send the changes">
   </fieldset>
-
-</form>
+</form>-->
