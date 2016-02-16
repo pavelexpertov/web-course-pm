10f0c4f6ae7be3f0b7906924b943636b00f2e76e
pavelexpertov <pavelm01@hotmail.co.uk>
2016-02-16 01:08:41 +0000

    started on the functions

---
+++ php/cms/verifydata/valid_serial_data.func.php
@@ -0,0 +1,28 @@
+<?php
+//The purpose of the file is to contain functions that
+//will check user input by first serialising it and then
+//validate it.
+
+function checkInt($number)
+{
+    $num = filter_var($number, FILTER_SANITIZE_NUMBER_INT);
+    return filter_var($num, FILTER_VALIDATE_INT);
+}
+
+function checkDate($date)
+{
+    //The $date is supposed to be in dd/mm/yyyy format
+}
+
+function checkString($string)
+{
+    $s = filter_var($string, FILTER_SANITIZE_STRING);
+    return $s;
+}
+
+function checkDate($date)
+{
+
+}
+
+ ?>
