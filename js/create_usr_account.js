d20a0150e39b2739f44ff5ecb5c7be68c7cf7188
pavelexpertov <pavelm01@hotmail.co.uk>
2016-02-19 10:41:29 +0000

    Added some rules to the password

--- js/create_usr_account.js
+++ js/create_usr_account.js
@@ -4,12 +4,20 @@ $("#create-usr-account").validate({
         usrname: {
             required: true,
             maxlength: 25
+        },
+        usrpwd: {
+            required: true,
+            maxlength: 25
         }
     },
     messages: {
         usrname: {
             required: "It needs to be filled in",
             maxlength: "Username must be 25 characters long"
+        },
+        usrpwd: {
+            required: "You must enter your password",
+            maxlength: "Your password must be 25 characters long"
         }
     }
 }//End of outer validate function
