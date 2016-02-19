b64a80597a429d3ff9ab1632c5b4937b4cf6233e
pavelexpertov <pavelm01@hotmail.co.uk>
2016-02-19 17:33:14 +0000

    Reformatted the form for creating a new venue

--- php/usr/includes/web_comp/create_venue_form.php
+++ php/usr/includes/web_comp/create_venue_form.php
@@ -1,17 +1,24 @@
+<?php include 'php/web_comp/button_element.func.php';?>
 <form action="php/cms/insert/create_venue_category.php" method="post">
     <fieldset>
       <legend>Create a new Venue</legend>
       <label>Venue name</label>
       <input type="text" id="vname" name="vname">
+      <br>
       <label>Town</label>
       <input type="text" id="town" name="town">
+      <br>
       <label>Country</label>
       <input type="text" id="country" name="country">
+      <br>
       <label>Venue's address</label>
-      <textarea id="addrss" name="addrss"> </textarea>
+      <br>
+      <textarea id="addrss" name="addrss"></textarea>
+      <br>
       <label>Capacity</label>
       <input type="text" id="cap" name="cap">
-      <input type="submit" value="Create a new event">
-      <input type="hidden" name="usrid" value="<?php echo $_SESSION['usr']->id;?>">
+      <br>
+      <input type="submit" value="Create new venue">
+      <?php placeButton("Cancel", "usr_page.php");?>
     </fieldset>
 </form>
