7672dbeeddd695a4bf11962ed5ba001fb0f0b51f
pavelexpertov <pavelm01@hotmail.co.uk>
2016-02-19 16:34:41 +0000

    Made it so the user can see different functionalities

--- php/usr/includes/sections/usrpage_section.inc.php
+++ php/usr/includes/sections/usrpage_section.inc.php
@@ -1,18 +1,26 @@
 <div class="middle-section">
 <div id="tabs">
     <ul>
-        <li><a href="#tabs-1">Created Events</a></li>
+        <?php
+        if($_SESSION['usr']->eveadmin)
+            echo '<li><a href="#tabs-1">Created Events</a></li>';
+        ?>
         <li><a href="#tabs-2">Booked Events</a></li>
-        <li><a href="#tabs-3">Manage Categories</a></li>
+        <?php
+        if($_SESSION['usr']->eveadmin)
+            echo '<li><a href="#tabs-3">Manage Categories</a></li>';
+        ?>
         <li><a href="#tabs-4">User Settings</a></li>
     </ul>
 <?php
     //The section is used for the user to edit events, manage
     //bookings and edit user settings.
     //Includes a list of created events
-    include 'php/usr/includes/web_comp/createdeventslist.inc.php';
+    if($_SESSION['usr']->eveadmin)
+        include 'php/usr/includes/web_comp/createdeventslist.inc.php';
     include 'php/usr/includes/web_comp/bookedeventslist.inc.php';
-    include 'php/usr/includes/web_comp/categorylist.inc.php';
+    if($_SESSION['usr']->eveadmin)
+        include 'php/usr/includes/web_comp/categorylist.inc.php';
     include 'php/usr/includes/web_comp/usersettings.inc.php';
  ?>
 </div>
