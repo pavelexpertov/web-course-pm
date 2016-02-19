0f694dfd5f28be8156a03b08fa9cae89996d7cf4
pavelexpertov <pavelm01@hotmail.co.uk>
2016-02-19 17:00:44 +0000

    Reformated form for creating an event type

--- php/usr/includes/web_comp/create_event_type_form.php
+++ php/usr/includes/web_comp/create_event_type_form.php
@@ -1,11 +1,15 @@
+<?php include 'php/web_comp/button_element.func.php';?>
 <form action="php/cms/insert/create_event_type_category.php" method="post">
     <fieldset>
       <legend>Create a new Event Type</legend>
       <label>Event Type name</label>
       <input type="text" id="ename" name="ename">
+      <br>
       <label>Event's description</label>
-      <textarea id="descr" name="descr"> </textarea>
+      <br>
+      <textarea id="descr" name="descr"></textarea>
+      <br>
       <input type="submit" value="Create a new venue">
-      <input type="hidden" name="usrid" value="<?php echo $_SESSION['usr']->id;?>">
+      <?php placeButton("Cancel", "usr_page.php");?>
     </fieldset>
 </form>
