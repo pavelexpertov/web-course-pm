26dffc76690c8e2d4932e5e27a6988202ec2272a
pavelexpertov <pavelm01@hotmail.co.uk>
2016-02-19 17:05:10 +0000

    Edited the form for the event type

--- php/usr/includes/web_comp/edit_event_type_form.inc.php
+++ php/usr/includes/web_comp/edit_event_type_form.inc.php
@@ -2,6 +2,12 @@
   //The purpose of the page is to get information about existing event type
   //and enter it into the form field elements for the user to edit
   //Creating a query
+  include 'php/cms/verifydata/valid_serial_data.func.php';
+  include 'php/web_comp/button_element.func.php';
+  $etid = checkInt($_GET['etid']);
+  $listOfVars = array($etid);
+  include 'php/cms/verifydata/check_for_false_vars.inc.php';
+
   $query = "select Name, Description from EventTypes where ID = ?";
   $stmt = $mysqli->prepare($query);
   if($stmt == false)
@@ -11,7 +17,7 @@
     //echo $stmt->error_list;
     exit();
   }
-  $stmt->bind_param('i', $_GET['etid']);
+  $stmt->bind_param('i', $etid);
   $stmt->execute();
 
   //Checking for a number of queries
@@ -33,9 +39,13 @@
       <legend>Edit a new Event Type</legend>
       <label>Event Type name</label>
       <input type="text" id="ename" name="ename" value="<?php echo $name;?>">
+      <br>
       <label>Event's description</label>
-      <textarea id="descr" name="descr"> <?php echo $descr;?></textarea>
+      <br>
+      <textarea id="descr" name="descr"><?php echo $descr;?></textarea>
+      <br>
       <input type="submit" value="Save Changes">
-      <input type="hidden" name="etid" value="<?php echo $_GET['etid'];?>">
+      <?php placeButton("Cancel", "usr_page.php");?>
+      <input type="hidden" name="etid" value="<?php echo $etid;?>">
     </fieldset>
 </form>
