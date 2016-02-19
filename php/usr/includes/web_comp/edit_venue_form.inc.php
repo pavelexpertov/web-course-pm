f17829e61b8e56bbd29d2dcfd3af43bbfe1f3b50
pavelexpertov <pavelm01@hotmail.co.uk>
2016-02-19 18:01:50 +0000

    Reformated edit venue form

--- php/usr/includes/web_comp/edit_venue_form.inc.php
+++ php/usr/includes/web_comp/edit_venue_form.inc.php
@@ -1,18 +1,13 @@
-<!-- <form action="php/cms/update/update_created_event.php" method="post">
-  <fieldset>
-    <legend>Edit your event</legend>
-    <label> Event Name</label>
-    <input type="text" value = "This is the name to edit for" >
-    <textarea>This is a text to edit </textarea>
-    <input type="submit" value="Send the changes">
-  </fieldset>
-
-</form> -->
-
 <?php
   //The purpose of the page is to get information about existing event
   //and enter it into the form field elements for the user to edit
   //Creating a query
+  include 'php/cms/verifydata/valid_serial_data.func.php';
+  include 'php/web_comp/button_element.func.php';
+  $vid = checkInt($_GET['vid']);
+  $listOfVars = array($vid);
+  include 'php/cms/verifydata/check_for_false_vars.inc.php';
+
   $query = "select Name, Address, Town, Country, Capacity
             from Venues where ID = ?";
   $stmt = $mysqli->prepare($query);
@@ -22,7 +17,7 @@
     echo $stmt->error_list;
     exit();
   }
-  $stmt->bind_param('i', $_GET['vid']);
+  $stmt->bind_param('i', $vid);
   $stmt->execute();
 
   //Checking for a number of queries
@@ -44,15 +39,23 @@
       <legend>Edit Venue</legend>
       <label>Venue name</label>
       <input type="text" id="vname" name="vname" value="<?php echo $name; ?>">
+      <br>
       <label>Town</label>
       <input type="text" id="town" name="town" value="<?php echo $town;?>">
+      <br>
       <label>Country</label>
       <input type="text" id="country" name="country" value="<?php echo $country;?>">
+      <br>
       <label>Venue's address</label>
-      <textarea id="addrss" name="addrss"> <?php echo $add;?></textarea>
+      <br>
+      <textarea id="addrss" name="addrss"><?php echo $add;?></textarea>
+      <br>
       <label>Capacity</label>
       <input type="text" id="cap" name="cap" value="<?php echo $cap;?>">
+      <br>
       <input type="submit" value="Create a new event">
-      <input type="hidden" name="vid" value="<?php echo $_GET['vid'];?>">
+      <br>
+      <?php placeButton("Cancel", "usr_page.php");?>
+      <input type="hidden" name="vid" value="<?php echo $vid;?>">
     </fieldset>
 </form>
