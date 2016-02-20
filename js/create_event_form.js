2cf8e3cc102e6109b2cdd9d06b647d98cf5f35e9
pavelexpertov <pavelm01@hotmail.co.uk>
2016-02-20 14:13:24 +0000

    Created a new rule to check the times

--- js/create_event_form.js
+++ js/create_event_form.js
@@ -1,3 +1,25 @@
+        /*var stime = $("#stime option:selected").val();
+        var ftime = $("#ftime option:selected").val();
+        var stime = stime.split(":");
+        var ftime = ftime.split(":");
+        var stime = (Number(stime[0])*60) + Number(stime[1]);
+        var ftime = (Number(ftime[0])*60) + Number(ftime[1]);
+        alert((stime < ftime));*/
+//alert($('#ftime').text);
+//alert($("#ftime option:selected").val());
+$.validator.addMethod("usrnameRegex", function(value, element) {
+       return this.optional(element) || /^[a-zA-Z]\w{1,24}$/i.test(value);
+}, "Username must contain only letters, numbers, or dashes.");
+$.validator.addMethod("checkDate", function(value, element){
+        var stime = $("#stime option:selected").val();
+        var ftime = $("#ftime option:selected").val();
+        var stime = stime.split(":");
+        var ftime = ftime.split(":");
+        var stime = (Number(stime[0])*60) + Number(stime[1]);
+        var ftime = (Number(ftime[0])*60) + Number(ftime[1]);
+        return (stime < ftime);
+}, "The start time needs to be earlier than finish time!");
+
 //Adding validation rules
 $("#create-event").validate({
     rules: {
@@ -9,6 +31,9 @@ $("#create-event").validate({
         date: {
             required: true,
             date: true
+        },
+        stime: {
+            checkDate: true
         }
     },
     messages: {
