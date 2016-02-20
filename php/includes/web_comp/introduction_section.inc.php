6c03c404e043b14cc00b96862762b1d4df9520c3
pavelexpertov <pavelm01@hotmail.co.uk>
2016-02-20 17:25:42 +0000

    Edited the intro to fit the themee

--- php/includes/web_comp/introduction_section.inc.php
+++ php/includes/web_comp/introduction_section.inc.php
@@ -9,15 +9,15 @@
 <p> Here are some event types you most likely to find out.</p>
 <h2>Conference</h2>
 <img alt="The conference image is missing" src="img/conf.jpg" height="245" width="375">
-<p>Conferences are events where a certain number of speakers come together to
+<p class="descr">Conferences are events where a certain number of speakers come together to
     talk about certain technologies.</p>
 <h2>Workshops</h2>
 <img alt="The conference image is missing" src="img/work.jpg" height="245" width="375">
-<p>Workshops are events where people can come together and do certain activities to gain
+<p class="descr">Workshops are events where people can come together and do certain activities to gain
 some skills.</p>
 <h2>Hackathon</h2>
 <img alt="The conference image is missing" src="img/hack.jpg" height="245" width="375">
-<p>This event brings people together to do a week long sprint to develop
+<p class="descr">This event brings people together to do a week long sprint to develop
 different ideas that can improve businesses or communities.</p>
 
 
