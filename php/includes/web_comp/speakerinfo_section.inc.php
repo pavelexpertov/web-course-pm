3b0afb64d6669a0a01f6d8122960b87c01bc1b41
pavelexpertov <pavelm01@hotmail.co.uk>
2016-02-20 13:23:32 +0000

    changed how the info is selected from user info.

--- php/includes/web_comp/speakerinfo_section.inc.php
+++ php/includes/web_comp/speakerinfo_section.inc.php
@@ -1,6 +1,12 @@
 <section>
     <?php
-    $query = "select fname, lname, job, descr from speaker_info where id = ?";
+    include_once 'php/cms/verifydata/valid_serial_data.func.php';
+    $id = checkInt($_GET['id']);
+    $listOfVars = array($id);
+    include_once 'php/cms/verifydata/check_for_false_vars.inc.php';
+
+    //$query = "select fname, lname, job, descr from speaker_info where id = ?";
+    $query = "select FirstName, LastName, CurrentJob, Description from Users where ID = ? and EventAdmin = 1";
     $result = $mysqli->prepare($query);
 
     if($result == false)
@@ -9,7 +15,7 @@
         echo $result->error_list;
     }
 
-    $result->bind_param('i', $_GET['id']);
+    $result->bind_param('i', $id);
     $result->execute();
     $result->bind_result($fname, $lname, $job, $descr);
     $result->fetch();
