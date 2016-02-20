565b2b5c15e2bb8382694effd3786561cf5561ed
pavelexpertov <pavelm01@hotmail.co.uk>
2016-02-20 16:43:46 +0000

    Added images and intro text to introduction section

--- php/includes/web_comp/introduction_section.inc.php
+++ php/includes/web_comp/introduction_section.inc.php
@@ -1,10 +1,24 @@
 <section id="introduction">
     <!-- This section represents different types of events
     that are stored within small boxes -->
+    <h1>Welcome to CoderCon!</h1>
     <p>
-        Welcome to the CoderCon! Here you can book tickets for workshops and conferences that
+        Here you can book tickets for workshops and conferences that
 	are made for developers, technical administrators and tech hobbyists.
     </p>
+<p> Here are some event types you most likely to find out.</p>
+<h2>Conference</h2>
+<img alt="The conference image is missing" src="img/conf.jpg" height="245" width="375">
+<p>Conferences are events where a certain number of speakers come together to
+    talk about certain technologies.</p>
+<h2>Workshops</h2>
+<img alt="The conference image is missing" src="img/work.jpg" height="245" width="375">
+<p>Workshops are events where people can come together and do certain activities to gain
+some skills.</p>
+<h2>Hackathon</h2>
+<img alt="The conference image is missing" src="img/hack.jpg" height="245" width="375">
+<p>This event brings people together to do a week long sprint to develop
+different ideas that can improve businesses or communities.</p>
 
 
 </section>
