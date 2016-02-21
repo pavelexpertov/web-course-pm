b82f85bdd56f17d9b71e7ac72d1757fb3d99da7f
pavelexpertov <pavelm01@hotmail.co.uk>
2016-02-21 23:46:40 +0000

    Finally displaying session variables

--- php/conn_sess/db_n_sess.inc.php
+++ php/conn_sess/db_n_sess.inc.php
@@ -1,6 +1,6 @@
 <?php
-//include 'php/debug/turn_on_error_reporting.php';
 include 'php/obj/allobj_files.inc.php';
 include 'php/conn_sess/sess.inc.php';
 include 'php/conn_sess/dbconn.inc.php';
+include 'php/debug/turn_on_error_reporting.php';
 ?>
