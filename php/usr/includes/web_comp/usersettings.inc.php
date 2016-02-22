9a661b1694b2ac3c514abeb7a649705b399b2a16
pavelexpertov <pavelm01@hotmail.co.uk>
2016-02-22 00:49:41 +0000

    Made so su isnt able to switch admin rights since he is root

--- php/usr/includes/web_comp/usersettings.inc.php
+++ php/usr/includes/web_comp/usersettings.inc.php
@@ -5,10 +5,13 @@
 	include_once "php/web_comp/button_element.func.php";
 	placeWideButton("Change your password", "usr_settings.php?pwd=");
 	placeWideButton("Change your details", "usr_settings.php?usrinfo=");
+	if($_SESSION['usr']->eveadmin != 2)
+	{
 	if($_SESSION['usr']->eveadmin)
 		placeWideButton("Deactivate Admin Features", "php/cms/update/toggling_admin_rights.php");
 	else
 		placeWideButton("Activate Admin Features", "php/cms/update/toggling_admin_rights.php");
+	}
 
 	?>
 </div>
