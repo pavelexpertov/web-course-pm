5bc94be8418b27f85bd3654b94a9cc6eee069a1d
pavelexpertov <pavelm01@hotmail.co.uk>
2016-02-22 02:35:47 +0000

    Turned off debug featres

--- php/conn_sess/db_n_sess.inc.php
+++ php/conn_sess/db_n_sess.inc.php
@@ -2,5 +2,5 @@
 include 'php/obj/allobj_files.inc.php';
 include 'php/conn_sess/sess.inc.php';
 include 'php/conn_sess/dbconn.inc.php';
-include 'php/debug/turn_on_error_reporting.php';
+//include 'php/debug/turn_on_error_reporting.php';
 ?>
