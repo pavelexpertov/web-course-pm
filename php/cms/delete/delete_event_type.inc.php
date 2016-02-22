4ef928411407fa005b3e5c0eef813fb975cf55ad
pavelexpertov <pavelm01@hotmail.co.uk>
2016-02-22 00:10:12 +0000

    Added ability to restore and delete event types

--- php/cms/delete/delete_event_type.inc.php
+++ php/cms/delete/delete_event_type.inc.php
@@ -17,6 +17,8 @@ include '../../cms/verifydata/check_for_false_vars.inc.php';
 //--Just assign 1 to booked event's Archived and that's it.
 //--Return user back to where he came from
 
+if($_SESSION['usr']->eveadmin != 2)
+{
 $query = "update EventTypes set Archived = 1
           where ID = ? and ManagerID = ?";
 
@@ -29,6 +31,22 @@ if($stmt == false)
 $stmt->bind_param("ii", $etid, $_SESSION['usr']->id);
 $stmt->execute();
 $stmt->close();
+}
+else
+{
+$query = "update EventTypes set Archived = 1
+          where ID = ?";
+
+$stmt = $mysqli->prepare($query);
+if($stmt == false)
+{
+    echo "Ooop an error has happened at prepare statement line";
+    exit();
+}
+$stmt->bind_param("i", $etid);
+$stmt->execute();
+$stmt->close();
+}
 //Return the user to the userpage
 header("Location: {$_SERVER['HTTP_REFERER']}");
 exit();
